1cf9bf48f83fee01520579a855886ebf2adef943
Sundeep Charan Ramkumar <sundeep-imac@Sundeeps-iMac.local>
2021-01-02 13:30:32 +0530

    Common config added inside webpack

---
+++ marketing/config/webpack.common.ts
@@ -0,0 +1,31 @@
+import { Configuration } from "webpack";
+
+const config: Configuration = {
+  module: {
+    rules: [
+      // TypeScript
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      // Babel (for JSX and React as well as Transpiling)
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+};
+
+module.exports = config;

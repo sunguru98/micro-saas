8b50000f731bf2f7a0de48bbf6ced60111b08d19
Sundeep Charan Ramkumar <webdevdesign@sundeepcharan.com>
2021-01-03 19:58:04 +0530

    Added random class name prefix for container project

--- packages/container/src/bootstrap.tsx
+++ packages/container/src/bootstrap.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 
+const randomClassName = createGenerateClassName({
+  productionPrefix: "container-",
+});
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <StylesProvider generateClassName={randomClassName}>
+    <Router>
+      <App />
+    </Router>
+  </StylesProvider>,
   document.querySelector("#root")
 );

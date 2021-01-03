045c44eb594ce56426c993c21af5b54fc5aa405b
Sundeep Charan Ramkumar <webdevdesign@sundeepcharan.com>
2021-01-03 19:52:50 +0530

    Included header component as well as wired with respect to container project

--- packages/container/src/App.tsx
+++ packages/container/src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import Header from "./components/Header";
 import MarketingIndex from "./components/MarketingIndex";
 
 const App = () => {
-  return <MarketingIndex />;
+  return (
+    <div>
+      <Header />
+      <MarketingIndex />
+    </div>
+  );
 };
 
 export default App;

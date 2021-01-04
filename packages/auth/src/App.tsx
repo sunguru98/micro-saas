5e00fa2f7696fb192e703dfd9c669e3dc50415cb
Sundeep Charan Ramkumar <webdevdesign@sundeepcharan.com>
2021-01-04 15:26:53 +0530

    Wired both pages inside App.tsx

--- packages/auth/src/App.tsx
+++ packages/auth/src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import SignIn from "./components/Signin";
+import SignUp from "./components/Signup";
 
 const App: React.FC = () => {
-  return <h1>I am an Auth element</h1>;
+  return (
+    <Switch>
+      <Route exact path="/auth/signin" component={SignIn} />
+      <Route exact path="/auth/signup" component={SignUp} />
+    </Switch>
+  );
 };
 
 export default App;

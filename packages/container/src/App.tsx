23a892ae8c4bffac62eed0a025f22f1cb32218b8
Sundeep Charan Ramkumar <webdevdesign@sundeepcharan.com>
2021-01-09 19:05:50 +0530

    Added lazy loading for auth and marketing app

--- packages/container/src/App.tsx
+++ packages/container/src/App.tsx
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
 
 // Federated modules;
-import AuthIndex from "./components/AuthIndex";
-import MarketingIndex from "./components/MarketingIndex";
+const AuthIndex = lazy(() => import("./components/AuthIndex"));
+const MarketingIndex = lazy(() => import("./components/MarketingIndex"));
 
 const App: React.FC = () => {
   return (
     <div>
       <Header />
-      <Switch>
-        <Route path="/auth" component={AuthIndex} />
-        <Route path="/" component={MarketingIndex} />
-      </Switch>
+      <Suspense fallback={<h1>Loading ...</h1>}>
+        <Switch>
+          <Route path="/auth" component={AuthIndex} />
+          <Route path="/" component={MarketingIndex} />
+        </Switch>
+      </Suspense>
     </div>
   );
 };

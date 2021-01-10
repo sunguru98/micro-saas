95bf50a5379a6ec0e12d1bc6ca4103f582dd51ff
Sundeep Charan Ramkumar <webdevdesign@sundeepcharan.com>
2021-01-10 18:54:50 +0530

    Made some changes on webpack config to match with Vue.js

--- packages/dashboard/config/webpack.common.ts
+++ packages/dashboard/config/webpack.common.ts
@@ -1,29 +1,35 @@
 import { Configuration, WebpackPluginInstance } from "webpack";
 import { CleanWebpackPlugin } from "clean-webpack-plugin";
+import { VueLoaderPlugin } from "vue-loader";
 
-const plugins: WebpackPluginInstance[] = [new CleanWebpackPlugin()];
+const plugins: WebpackPluginInstance[] = [
+  new CleanWebpackPlugin(),
+  new VueLoaderPlugin(),
+];
 
 const config: Configuration = {
-  entry: "./src/index.ts",
+  entry: "./src/index.js",
   module: {
     rules: [
+      { test: /\.(png|jpe?g|gif|woff|svg|eot|ttf)$/i, loader: "file-loader" },
+      { test: /\.vue$/, loader: "vue-loader" },
       {
-        test: /\.(t|j)sx?$/,
+        test: /\.s?css$/,
+        use: ["vue-style-loader", "sass-loader", "css-loader", "style-loader"],
+      },
+      {
+        test: /\.m?js$/,
         loader: "babel-loader",
         exclude: /node_modules/,
         options: {
-          presets: [
-            "@babel/preset-typescript",
-            "@babel/preset-env",
-            "@babel/preset-react",
-          ],
+          presets: ["@babel/preset-env"],
           plugins: ["@babel/plugin-transform-runtime"],
         },
       },
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: [".vue", ".ts", ".js"],
   },
   plugins,
 };
--- packages/dashboard/src/bootstrap.ts
+++ packages/dashboard/src/bootstrap.ts
@@ -1,1 +1,3 @@
-export default "";
+const mount = (element: Element) => {};
+
+export { mount };
